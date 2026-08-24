d41a949ff8096e7481e7bcb6ccbb6d8babf5b71e
Harini .V <harinisathya024@gmail.com>
2026-08-24 21:56:22 +0530

    Delete todo directory

--- todo/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns=[
-    path('',views.todo,name='todo'),
-    path('update/<int:id>',views.Edit,name='Edit'),
-    path('delete/<int:id>',views.Delete,name='Delete'),
-]
